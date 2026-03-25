36dab4ec6fd3909c0fa6ff26a65cfa2c1c6a5046
vclorenzo <vc.lorenzo16@gmail.com>
2026-03-26 02:26:11 +0800

    added arcjet security middleware

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js';
+import securityMiddleware from '#middleware/security.middleware.js';
 
 const app = express();
 app.use(helmet());
@@ -12,12 +13,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
 app.use(
   morgan('combined', {
     stream: { write: message => logger.info(message.trim()) },
   })
 );
+app.use(securityMiddleware);
 
 app.get('/', (req, res) => {
   logger.info('Hello from Acquisitions');
